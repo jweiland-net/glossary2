532ddc5ddc83945767e22473bcb96035dea36606
Stefan Froemken <froemken@gmail.com>
2018-02-02 08:48:40 +0100

    Update TYPO3 TCA fields to new version

--- Configuration/TCA/tx_glossary2_domain_model_glossary.php
+++ Configuration/TCA/tx_glossary2_domain_model_glossary.php
@@ -1,15 +1,12 @@
 <?php
-// in 8.5 some locallang_general.xlf has been moved into Resources/Private/Language
-if (version_compare(TYPO3_version, '8.5', '>=')) {
-    $locallangGeneral = 'LLL:EXT:lang/Resources/Private/Language/locallang_general.xlf:';
+if (\TYPO3\CMS\Core\Utility\GeneralUtility::compat_version('7.6')) {
+    $ttContentLanguageFile = 'LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf';
 } else {
-    $locallangGeneral = 'LLL:EXT:lang/locallang_general.xlf:';
+    $ttContentLanguageFile = 'LLL:EXT:cms/locallang_ttc.xlf';
 }
-$locallangTtc = 'LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:';
-
 return [
     'ctrl' => [
-        'title'	=> 'LLL:EXT:glossary2/Resources/Private/Language/locallang_db.xlf:tx_glossary2_domain_model_glossary',
+        'title' => 'LLL:EXT:glossary2/Resources/Private/Language/locallang_db.xlf:tx_glossary2_domain_model_glossary',
         'label' => 'title',
         'tstamp' => 'tstamp',
         'crdate' => 'crdate',
@@ -35,15 +32,15 @@ return [
     ],
     'columns' => [
         'sys_language_uid' => [
-            'exclude' => true,
-            'label' => $locallangGeneral . 'LGL.language',
+            'exclude' => 1,
+            'label' => 'LLL:EXT:lang/locallang_general.xlf:LGL.language',
             'config' => [
                 'type' => 'select',
                 'renderType' => 'selectSingle',
                 'special' => 'languages',
                 'items' => [
                     [
-                        $locallangGeneral . 'LGL.allLanguages',
+                        'LLL:EXT:lang/locallang_general.xlf:LGL.allLanguages',
                         -1,
                         'flags-multiple'
                     ],
@@ -52,69 +49,75 @@ return [
             ]
         ],
         'l10n_parent' => [
-            'displayCond' => 'FIELD:sys_language_uid:>:0',
             'exclude' => 1,
-            'label' => $locallangGeneral . 'LGL.l18n_parent',
+            'displayCond' => 'FIELD:sys_language_uid:>:0',
+            'label' => 'LLL:EXT:lang/locallang_general.xlf:LGL.l18n_parent',
             'config' => [
                 'type' => 'select',
+                'renderType' => 'selectSingle',
                 'items' => [
-                    ['', 0],
+                    [
+                        '',
+                        0
+                    ]
                 ],
-                'foreign_table' => 'tx_glossary2_domain_model_glossary',
-                'foreign_table_where' => 'AND tx_glossary2_domain_model_glossary.pid=###CURRENT_PID### AND tx_glossary2_domain_model_glossary.sys_language_uid IN (-1,0)',
-            ],
+                'foreign_table' => 'tt_content',
+                'foreign_table_where' => 'AND tt_content.pid=###CURRENT_PID### AND tt_content.sys_language_uid IN (-1,0)',
+                'default' => 0
+            ]
         ],
         'l10n_diffsource' => [
             'config' => [
                 'type' => 'passthrough',
-            ],
+                'default' => ''
+            ]
         ],
         't3ver_label' => [
-            'label' => $locallangGeneral . 'LGL.versionLabel',
+            'label' => 'LLL:EXT:lang/locallang_general.xlf:LGL.versionLabel',
             'config' => [
                 'type' => 'input',
-                'size' => 30,
-                'max' => 255,
+                'size' => '30',
+                'max' => '255'
             ]
         ],
         'hidden' => [
             'exclude' => 1,
-            'label' => $locallangGeneral . 'LGL.hidden',
+            'label' => 'LLL:EXT:lang/locallang_general.xlf:LGL.hidden',
             'config' => [
                 'type' => 'check',
-            ],
+                'items' => [
+                    '1' => [
+                        '0' => 'LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:hidden.I.0'
+                    ]
+                ]
+            ]
         ],
         'starttime' => [
             'exclude' => 1,
-            'l10n_mode' => 'mergeIfNotBlank',
-            'label' => $locallangGeneral . 'LGL.starttime',
+            'label' => 'LLL:EXT:lang/locallang_general.xlf:LGL.starttime',
             'config' => [
                 'type' => 'input',
-                'size' => 13,
-                'max' => 20,
+                'size' => '13',
                 'eval' => 'datetime',
-                'checkbox' => 0,
-                'default' => 0,
-                'range' => [
-                    'lower' => mktime(0, 0, 0, date('m'), date('d'), date('Y'))
-                ],
+                'default' => 0
             ],
+            'l10n_mode' => 'exclude',
+            'l10n_display' => 'defaultAsReadonly'
         ],
         'endtime' => [
             'exclude' => 1,
-            'l10n_mode' => 'mergeIfNotBlank',
-            'label' => $locallangGeneral . 'LGL.endtime',
+            'label' => 'LLL:EXT:lang/locallang_general.xlf:LGL.endtime',
             'config' => [
                 'type' => 'input',
-                'size' => 13,
-                'max' => 20,
+                'size' => '13',
                 'eval' => 'datetime',
-                'checkbox' => 0,
                 'default' => 0,
                 'range' => [
-                    'lower' => mktime(0, 0, 0, date('m'), date('d'), date('Y'))
-                ],
+                    'upper' => mktime(0, 0, 0, 1, 1, 2038)
+                ]
             ],
+            'l10n_mode' => 'exclude',
+            'l10n_display' => 'defaultAsReadonly'
         ],
         'title' => [
             'exclude' => 1,
@@ -163,7 +166,7 @@ return [
         ],
     ],
     'types' => [
-        '1' => ['showitem' => 'sys_language_uid;;;;1-1-1, l10n_parent, l10n_diffsource, hidden;;1, title, description, images,--div--;' . $locallangTtc .'tabs.access,starttime, endtime'],
+        '1' => ['showitem' => 'sys_language_uid;;;;1-1-1, l10n_parent, l10n_diffsource, hidden;;1, title, description, images,--div--;' . $ttContentLanguageFile . ':tabs.access,starttime, endtime'],
     ],
     'palettes' => [
         '1' => ['showitem' => ''],
