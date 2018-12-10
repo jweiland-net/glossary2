b9a8038a5114c794761ad54583d846ed9113c4da
Stefan Froemken <froemken@gmail.com>
2018-12-10 14:31:56 +0100

    Add compatibility for TYPO3 9

--- ext_emconf.php
+++ ext_emconf.php
@@ -27,10 +27,10 @@ $EM_CONF[$_EXTKEY] = [
     'modify_tables' => '',
     'clearCacheOnLoad' => 0,
     'lockType' => '',
-    'version' => '2.0.1',
+    'version' => '2.1.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '7.6.0-8.7.99',
+            'typo3' => '8.7.0-9.5.99',
         ],
         'conflicts' => [
         ],
