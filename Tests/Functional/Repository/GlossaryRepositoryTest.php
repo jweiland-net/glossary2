44a9e3add17428387e53a2df1a065cd73af77555
Stefan Froemken <froemken@gmail.com>
2020-08-17 16:07:22 +0200

    Add further tests for Fluid link generation

--- Tests/Functional/Repository/GlossaryRepositoryTest.php
+++ Tests/Functional/Repository/GlossaryRepositoryTest.php
@@ -2,6 +2,7 @@
 
 /*
  * This file is part of the package jweiland/glossary2.
+ *
  * For the full copyright and license information, please read the
  * LICENSE file that was distributed with this source code.
  */
