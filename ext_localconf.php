adbb098ff005b445c9e697f842a2eaf0e7feb129
Stefan Froemken <froemken@gmail.com>
2021-09-17 10:23:01 +0200

    Remove FlexForm updater for TYPO3 8 and 9

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,9 +3,9 @@ if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 
-call_user_func(function ($extKey) {
+call_user_func(static function () {
     \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-        'JWeiland.' . $extKey,
+        'JWeiland.glossary2',
         'Glossary',
         array(
             'Glossary' => 'list, listWithoutGlossar, show',
@@ -34,10 +34,6 @@ call_user_func(function ($extKey) {
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:glossary2/Configuration/TSconfig/ContentElementWizard.txt">');
 
     // Update old flex form settings
-    if (version_compare(TYPO3_branch, '9.4', '>=')) {
-        $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/install']['update']['glossary2UpdateOldFlexFormFields'] = \JWeiland\Glossary2\Updater\MoveOldFlexFormSettings94Updater::class;
-        $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/install']['update']['glossary2UpdateSlug'] = \JWeiland\Glossary2\Updater\GlossarySlugUpdater::class;
-    } else {
-        $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/install']['update']['glossary2UpdateOldFlexFormFields'] = \JWeiland\Glossary2\Updater\MoveOldFlexFormSettings87Updater::class;
-    }
-}, 'glossary2');
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/install']['update']['glossary2UpdateOldFlexFormFields'] = \JWeiland\Glossary2\Updater\MoveOldFlexFormSettingsUpdater::class;
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/install']['update']['glossary2UpdateSlug'] = \JWeiland\Glossary2\Updater\GlossarySlugUpdater::class;
+});
