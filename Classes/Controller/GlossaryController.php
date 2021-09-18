943caeb139de4551762e5119cc47d14a84a0d8d2
Stefan Froemken <froemken@gmail.com>
2021-09-18 02:19:44 +0200

    Replace curly bracets with brackets

--- Classes/Controller/GlossaryController.php
+++ Classes/Controller/GlossaryController.php
@@ -82,7 +82,7 @@ class GlossaryController extends ActionController
      */
     public function showAction(Glossary $glossary): void
     {
-        $letter = strtr(mb_strtolower($glossary->getTitle(){0}), 'äöü', 'aou');
+        $letter = strtr(mb_strtolower($glossary->getTitle()[0]), 'äöü', 'aou');
         $this->view->assign('glossary', $glossary);
         $this->view->assign('letter', $letter);
     }
