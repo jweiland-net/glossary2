7c5343b2a6de41e2db3b1b471740407a7709f14d
Stefan Froemken <froemken@gmail.com>
2023-03-20 15:18:28 +0100

    Apply new format to ext_tables.sql

--- ext_tables.sql
+++ ext_tables.sql
@@ -1,9 +1,10 @@
 #
 # Table structure for table 'tx_glossary2_domain_model_glossary'
 #
-CREATE TABLE tx_glossary2_domain_model_glossary (
-	title varchar(255) DEFAULT '' NOT NULL,
+CREATE TABLE tx_glossary2_domain_model_glossary
+(
+	title        varchar(255)  DEFAULT '' NOT NULL,
 	path_segment varchar(2048) DEFAULT '' NOT NULL,
-	description text,
-	images varchar(255) DEFAULT '' NOT NULL
+	description  text,
+	images       varchar(255)  DEFAULT '' NOT NULL
 );
