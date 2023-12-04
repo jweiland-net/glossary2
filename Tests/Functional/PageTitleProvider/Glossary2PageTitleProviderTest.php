f12ef19228814777b5086c1a1d1144ff8d4b1a27
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2023-12-04 15:24:19 +0100

    [Task] added new tests for page title provider

---
+++ Tests/Functional/PageTitleProvider/Glossary2PageTitleProviderTest.php
@@ -0,0 +1,74 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the package jweiland/glossary2.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+namespace JWeiland\Glossary2\Tests\Functional\PageTitleProvider;
+
+use JWeiland\Glossary2\Domain\Model\Glossary;
+use JWeiland\Glossary2\Domain\Repository\GlossaryRepository;
+use JWeiland\Glossary2\PageTitleProvider\Glossary2PageTitleProvider;
+use TYPO3\TestingFramework\Core\Functional\FunctionalTestCase;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Persistence\Generic\QuerySettingsInterface;
+use TYPO3\CMS\Extbase\Persistence\PersistenceManagerInterface;
+
+/**
+ * Functional test for Glossary2PageTitleProvider
+ */
+class Glossary2PageTitleProviderTest extends FunctionalTestCase
+{
+    protected Glossary2PageTitleProvider $subject;
+
+    protected array $testExtensionsToLoad = [
+        'typo3conf/ext/glossary2'
+    ];
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $pageId = 15;
+        $persistenceManager = GeneralUtility::makeInstance(PersistenceManagerInterface::class);
+        $querySettings = GeneralUtility::makeInstance(QuerySettingsInterface::class);
+        $querySettings->setStoragePageIds([$pageId]);
+
+        $glossaryRepository = GeneralUtility::makeInstance(GlossaryRepository::class);
+        $glossaryRepository->setDefaultQuerySettings($querySettings);
+
+        $this->subject = new Glossary2PageTitleProvider($glossaryRepository);
+
+        $glossary = GeneralUtility::makeInstance(Glossary::class);
+        $glossary->setPid($pageId);
+        $glossary->setTitle('Nice title for detail page');
+        $glossary->setDescription('here is running a functional test case');
+        $persistenceManager->add($glossary);
+        $persistenceManager->persistAll();
+    }
+
+    protected function tearDown(): void
+    {
+        unset($this->pageTitleProvider);
+        parent::tearDown();
+    }
+
+    /**
+     * @test
+     */
+    public function getGlossaryDetailPageWithAssignedTitleShouldMatch(): void
+    {
+        $_GET['tx_glossary2_glossary']['action'] = 'show';
+        $_GET['tx_glossary2_glossary']['glossary'] = 1;
+
+        self::assertSame(
+            'Nice title for detail page',
+            $this->subject->getTitle()
+        );
+    }
+}
