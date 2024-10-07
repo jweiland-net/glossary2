222df2d7fbd8cd2695fc88e6b96c0650a760e821
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2024-10-07 15:50:24 +0200

    [TASK] Small TCA fixes and tweaks and documentation changes

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -1,17 +1,21 @@
 <?php
+
 if (!defined('TYPO3')) {
     die('Access denied.');
 }
 
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['glossary2_glossary'] = 'pi_flexform';
+use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
-    'glossary2_glossary',
-    'FILE:EXT:glossary2/Configuration/FlexForms/Glossary.xml'
-);
-
-\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+$pluginSignature = ExtensionUtility::registerPlugin(
     'glossary2',
     'Glossary',
     'LLL:EXT:glossary2/Resources/Private/Language/locallang_db.xlf:plugin.glossary.title'
+);
+
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['glossary2_glossary'] = 'pi_flexform';
+
+ExtensionManagementUtility::addPiFlexFormValue(
+    $pluginSignature,
+    'FILE:EXT:glossary2/Configuration/FlexForms/Glossary.xml'
 );
