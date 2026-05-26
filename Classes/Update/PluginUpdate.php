4d6de903175b3df2f26e9bedb91ae1653f026423
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2026-05-26 15:29:24 +0200

    [TASK] Update namespaces for UpgradeWizard and AbstractListTypeToCTypeUpdate
    Replaced deprecated TYPO3\CMS\Install usages with TYPO3\CMS\Core equivalents to align with updated TYPO3 standards.

--- Classes/Update/PluginUpdate.php
+++ Classes/Update/PluginUpdate.php
@@ -11,8 +11,8 @@ declare(strict_types=1);
 
 namespace JWeiland\Glossary2\Update;
 
-use TYPO3\CMS\Install\Attribute\UpgradeWizard;
-use TYPO3\CMS\Install\Updates\AbstractListTypeToCTypeUpdate;
+use TYPO3\CMS\Core\Attribute\UpgradeWizard;
+use TYPO3\CMS\Core\Upgrades\AbstractListTypeToCTypeUpdate;
 
 /**
  * Updater to fill empty slug columns of glossary records
