74ca77acd98582c1a82ba15086ca97e47e6be207
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2026-05-26 15:28:47 +0200

    [TASK] Use promoted properties in ModifyQueryOfSearchGlossariesEvent constructor
    Refactored the constructor to leverage PHP promoted properties, reducing boilerplate code.

--- Classes/Event/ModifyQueryOfSearchGlossariesEvent.php
+++ Classes/Event/ModifyQueryOfSearchGlossariesEvent.php
@@ -20,29 +20,11 @@ use TYPO3\CMS\Extbase\Persistence\QueryResultInterface;
 class ModifyQueryOfSearchGlossariesEvent
 {
     /**
-     * @var QueryResultInterface<int, Glossary>
-     */
-    protected QueryResultInterface $queryResult;
-
-    /**
-     * @var array<int>
-     */
-    protected array $categories = [];
-
-    protected string $letter = '';
-
-    /**
-     * @param QueryResultInterface<int, Glossary> $extbaseQuery
+     * @param QueryResultInterface<int, Glossary> $queryResult
      * @param array<int> $categories
      */
-    public function __construct(
-        QueryResultInterface $extbaseQuery,
-        array $categories,
-        string $letter,
-    ) {
-        $this->queryResult = $extbaseQuery;
-        $this->categories = $categories;
-        $this->letter = $letter;
+    public function __construct(protected QueryResultInterface $queryResult, protected array $categories, protected string $letter)
+    {
     }
 
     /**
