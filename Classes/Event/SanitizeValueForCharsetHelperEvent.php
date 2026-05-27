89708bcdc9d35636a92826b089f3c6b9fd424628
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2026-05-27 10:16:25 +0200

    [TASK] Inline SanitizeValueForCharsetHelperEvent constructor for simplicity
    Inlined the constructor definition to reduce redundancy and streamline the code structure.

--- Classes/Event/SanitizeValueForCharsetHelperEvent.php
+++ Classes/Event/SanitizeValueForCharsetHelperEvent.php
@@ -17,9 +17,7 @@ namespace JWeiland\Glossary2\Event;
  */
 class SanitizeValueForCharsetHelperEvent
 {
-    public function __construct(protected string $value)
-    {
-    }
+    public function __construct(protected string $value) {}
 
     public function getValue(): string
     {
