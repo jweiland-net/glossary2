9dd00b7b80d9674fffa45a7342271e3bc8e3bada
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2026-05-27 09:29:10 +0200

    [TASK] Add strict types and remove redundant `call_user_func` usage in TCA override configuration
    Declared strict types and simplified TCA override configuration by directly invoking methods, removing unnecessary `call_user_func` usage.

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 /*
  * This file is part of the package jweiland/glossary2.
  *
@@ -14,49 +16,47 @@ if (!defined('TYPO3')) {
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
 
-call_user_func(static function () {
-    $pluginSignature = ExtensionUtility::registerPlugin(
-        'glossary2',
-        'Glossary',
-        'LLL:EXT:glossary2/Resources/Private/Language/locallang_db.xlf:plugin.glossary.title',
-        'ext-glossary2-wizard-icon',
-        'plugins',
-        'LLL:EXT:glossary2/Resources/Private/Language/locallang_db.xlf:plugin.glossary.description',
-    );
+$pluginSignature = ExtensionUtility::registerPlugin(
+    'glossary2',
+    'Glossary',
+    'LLL:EXT:glossary2/Resources/Private/Language/locallang_db.xlf:plugin.glossary.title',
+    'ext-glossary2-wizard-icon',
+    'plugins',
+    'LLL:EXT:glossary2/Resources/Private/Language/locallang_db.xlf:plugin.glossary.description',
+);
 
-    ExtensionManagementUtility::addToAllTCAtypes(
-        'tt_content',
-        '--div--;Configuration,pi_flexform,',
-        $pluginSignature,
-        'after:subheader',
-    );
+ExtensionManagementUtility::addToAllTCAtypes(
+    'tt_content',
+    '--div--;Configuration,pi_flexform,',
+    $pluginSignature,
+    'after:subheader',
+);
 
-    ExtensionManagementUtility::addPiFlexFormValue(
-        '*',
-        'FILE:EXT:glossary2/Configuration/FlexForms/Glossary.xml',
-        $pluginSignature,
-    );
+ExtensionManagementUtility::addPiFlexFormValue(
+    '*',
+    'FILE:EXT:glossary2/Configuration/FlexForms/Glossary.xml',
+    $pluginSignature,
+);
 
-    $GLOBALS['TCA']['tt_content']['types']['glossary2_glossary']['showitem'] = '
-        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
-            --palette--;;general,
-            --palette--;;headers,
-        --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.plugin,
-            pi_flexform,
-            pages,
-            recursive,
-        --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.appearance,
-            --palette--;;frames,
-            --palette--;;appearanceLinks,
-        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:language,
-            --palette--;;language,
-        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:access,
-            --palette--;;hidden,
-            --palette--;;access,
-        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:categories,
-            categories,
-        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:notes,
-            rowDescription,
-        --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:extended,
-    ';
-});
+$GLOBALS['TCA']['tt_content']['types']['glossary2_glossary']['showitem'] = '
+    --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
+        --palette--;;general,
+        --palette--;;headers,
+    --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.plugin,
+        pi_flexform,
+        pages,
+        recursive,
+    --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.appearance,
+        --palette--;;frames,
+        --palette--;;appearanceLinks,
+    --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:language,
+        --palette--;;language,
+    --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:access,
+        --palette--;;hidden,
+        --palette--;;access,
+    --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:categories,
+        categories,
+    --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:notes,
+        rowDescription,
+    --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:extended,
+';
