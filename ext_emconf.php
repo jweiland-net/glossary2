e10aa0351dde800799cd148826fe418ae87948c4
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2026-06-08 14:03:02 +0200

    [TASK] Update TYPO3 dependency range to 14.3.0-14.3.99 in ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -18,7 +18,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '8.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '14.0.0-14.4.99',
+            'typo3' => '14.3.0-14.3.99',
         ],
         'conflicts' => [
         ],
