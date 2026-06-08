892e65af88f67687d9564b02ff7e97feb9e2add9
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2026-06-08 12:27:25 +0200

    [TASK] Remove duplicate semicolon in Lazy attribute import

--- Classes/Domain/Model/Glossary.php
+++ Classes/Domain/Model/Glossary.php
@@ -13,7 +13,7 @@ namespace JWeiland\Glossary2\Domain\Model;
 
 use JWeiland\Glossary2\Helper\CharsetHelper;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\Extbase\Attribute\ORM\Lazy;;
+use TYPO3\CMS\Extbase\Attribute\ORM\Lazy;
 use TYPO3\CMS\Extbase\Domain\Model\Category;
 use TYPO3\CMS\Extbase\Domain\Model\FileReference;
 use TYPO3\CMS\Extbase\DomainObject\AbstractEntity;
