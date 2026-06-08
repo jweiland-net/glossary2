491e79705d5219a0d76fb835d0b37bb3388c8c6d
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2026-06-08 14:47:35 +0200

    [TASK] Remove redundant TCA configuration for plugin glossary2

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -18,29 +18,5 @@ $pluginSignature = ExtensionUtility::registerPlugin(
     'ext-glossary2-wizard-icon',
     'plugins',
     'LLL:EXT:glossary2/Resources/Private/Language/locallang_db.xlf:plugin.glossary.description',
+    'FILE:EXT:glossary2/Configuration/FlexForms/Glossary.xml',
 );
-
-$GLOBALS['TCA']['tt_content']['types'][$pluginSignature]['columnsOverrides']['pi_flexform']['config']['ds'] = 'FILE:EXT:glossary2/Configuration/FlexForms/Glossary.xml';
-
-$GLOBALS['TCA']['tt_content']['types'][$pluginSignature]['showitem'] = '
-    --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
-        --palette--;;general,
-        --palette--;;headers,
-    --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.plugin,
-        pi_flexform,
-        pages,
-        recursive,
-    --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.appearance,
-        --palette--;;frames,
-        --palette--;;appearanceLinks,
-    --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:language,
-        --palette--;;language,
-    --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:access,
-        --palette--;;hidden,
-        --palette--;;access,
-    --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:categories,
-        categories,
-    --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:notes,
-        rowDescription,
-    --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:extended,
-';
